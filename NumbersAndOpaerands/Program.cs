f2897c10e8b475d90785b7e4db5c93397d61fbef
Pavel <balandala05@gmail.com>
2023-11-06 00:11:26 +0500

    Added MakeTokenList()

--- NumbersAndOpaerands/Program.cs
+++ NumbersAndOpaerands/Program.cs
@@ -11,99 +11,146 @@ static public class Programm
     {
         while (true)
         {
-            string input = Console.ReadLine().Replace(" ", "");
+            string input = Console.ReadLine().Replace(" ", "").Replace('.', ',');
             if (input == "")
                 break;
             else
             {
-                Console.WriteLine(string.Join(' ', ReturnOpPrioTupleList(input)));
-                Console.WriteLine(string.Join(' ', ReturnNumbersList(input)));
+                input += " ";
+                //Console.WriteLine(string.Join(' ', ReturnOpPrioTupleList(input)));
+                //Console.WriteLine(string.Join(' ', ReturnNumbersList(input)));
+                Console.WriteLine(string.Join(' ', ToRpn(input)));
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(CalculateInput(input));
+                //Console.WriteLine(CalculateInput(input));
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
 
-    static List<(char, int)> ReturnOpPrioTupleList(string input)
+    static List<object> MakeTokenList(string input)
     {
-        var availbleOperators = new List<char>() {'*', '/', '+', '-', '^'};
-        var operatorsList = new List<(char, int)>();
-        char op;
-        int prio = 0;
-        foreach (var element in input)
+        var tokenList = new List<object>();
+        string buff = "";
+        foreach (char element in input)
         {
-            if (availbleOperators.Contains(element))
+            if (Char.IsDigit(element) || element == ',')
+                buff += element;
+            else
             {
-                op = element;
-                if ((element == '*') || (element == '/'))
-                    operatorsList.Add((op, prio + 1));
-                else if (element == '^')
-                    operatorsList.Add((op, prio + 2));
-                else
-                operatorsList.Add((op, prio)); 
+                tokenList.Add((buff));
+                tokenList.Add(element);
+                buff = "";
             }
-            else if (element == '(')
-                prio += 3;
-            else if (element == ')')
-                prio -= 3;
         }
-        return operatorsList;
+        return tokenList;
     }
 
-    static List<double> ReturnNumbersList(string input)
+    static List<object> ToRpn(string input)
     {
-        string number = "";
-        var numbersList = new List<double>();
-        int index = -1;
-        char previousNotDigit = ' ';
-        foreach (var digit in input + " ")
+        var tokenList = MakeTokenList(input);
+        Stack<string> stack = new Stack<string>();
+        var rpnExpresssion = new List<object>();
+        var availbleOperators = new List<string>() {"(", "*", "/", "+", "-", ")"};
+        foreach (object element in tokenList)
         {
-            if (Char.IsDigit(digit))
+            if (double.TryParse(element.ToString(), out double result))
             {
-                number += digit;
+                rpnExpresssion.Add(result);
             }
-            else if (number != "")
+            if (availbleOperators.Contains(element.ToString()))
             {
-                if (previousNotDigit == '.' || previousNotDigit == ',')
-                    numbersList[index] += Math.Round(double.Parse(number) * Math.Pow(0.1, number.Length), number.Length);
-                else
+                stack.Push(element.ToString());
+                if (element.Equals(")") || element.Equals(" "))
                 {
-                    numbersList.Add(double.Parse((number)));
-                    index++;
+                    while (stack.Peek() != "(" && stack.Count != 0)
+                        rpnExpresssion.Add(stack.Pop());
+                    stack.Pop();
                 }
-                number = "";
-                previousNotDigit = digit;      
             }
-            else continue; 
+          
         }
-        return numbersList;
+        return rpnExpresssion;
     }
 
-    static double CalculateInput(string input)
-    {
-        var operators = ReturnOpPrioTupleList(input);
-        var numbers = ReturnNumbersList(input);
-        if (input[0] == '-')
-        {
-            numbers[0] *= -1;
-            operators.RemoveAt(0);
-        }
-        while (numbers.Count > 1)
-        {
-            int maxPrio = operators.Max(t => t.Item2);
-            int index = operators.FindIndex(t => t.Item2 == maxPrio);
-            numbers[index] = Calculate(operators[index].Item1, numbers[index], numbers[index + 1]); ;
-            numbers.RemoveAt(index + 1);
-            operators.RemoveAt(index);
-        }
-        return numbers[0];
-    }
+    //static List<(char, int)> ReturnOpPrioTupleList(string input)
+    //{
+    //    var availbleOperators = new List<char>() {'*', '/', '+', '-', '^'};
+    //    var operatorsList = new List<(char, int)>();
+    //    char op;
+    //    int prio = 0;
+    //    foreach (var element in input)
+    //    {
+    //        if (availbleOperators.Contains(element))
+    //        {
+    //            op = element;
+    //            if ((element == '*') || (element == '/'))
+    //                operatorsList.Add((op, prio + 1));
+    //            else if (element == '^')
+    //                operatorsList.Add((op, prio + 2));
+    //            else
+    //            operatorsList.Add((op, prio)); 
+    //        }
+    //        else if (element == '(')
+    //            prio += 3;
+    //        else if (element == ')')
+    //            prio -= 3;
+    //    }
+    //    return operatorsList;
+    //}
 
-    static double Calculate(char op, double num1, double num2)
+    //static List<double> ReturnNumbersList(string input)
+    //{
+    //    string number = "";
+    //    var numbersList = new List<double>();
+    //    int index = -1;
+    //    char previousNotDigit = ' ';
+    //    foreach (var digit in input + " ")
+    //    {
+    //        if (Char.IsDigit(digit))
+    //        {
+    //            number += digit;
+    //        }
+    //        else if (number != "")
+    //        {
+    //            if (previousNotDigit == '.' || previousNotDigit == ',')
+    //                numbersList[index] += Math.Round(double.Parse(number) * Math.Pow(0.1, number.Length), number.Length);
+    //            else
+    //            {
+    //                numbersList.Add(double.Parse((number)));
+    //                index++;
+    //            }
+    //            number = "";
+    //            previousNotDigit = digit;      
+    //        }
+    //        else continue; 
+    //    }
+    //    return numbersList;
+    //}
+
+    //static double CalculateInput(string input)
+    //{
+    //    var operators = ReturnOpPrioTupleList(input);
+    //    var numbers = ReturnNumbersList(input);
+    //    if (input[0] == '-')
+    //    {
+    //        numbers[0] *= -1;
+    //        operators.RemoveAt(0);
+    //    }
+    //    while (numbers.Count > 1)
+    //    {
+    //        int maxPrio = operators.Max(t => t.Item2);
+    //        int index = operators.FindIndex(t => t.Item2 == maxPrio);
+    //        numbers[index] = Calculate(operators[index].Item1, numbers[index], numbers[index + 1]); ;
+    //        numbers.RemoveAt(index + 1);
+    //        operators.RemoveAt(index);
+    //    }
+    //    return numbers[0];
+    //}
+
+    static double Calculate(string op, double num1, double num2)
     {
-        switch (op)
+        switch (char.Parse(op))
         {
             case '+': return num1 + num2;
             case '-': return num1 - num2;
